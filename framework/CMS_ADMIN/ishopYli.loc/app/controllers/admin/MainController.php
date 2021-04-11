2204cdfb077363816a4978466c2be217387522d0
chenagoold <chenagoold@gmail.com>
2021-04-11 12:08:25 +0300

    11.04.21

---
+++ framework/CMS_ADMIN/ishopYli.loc/app/controllers/admin/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace app\controllers\admin;
+
+
+class MainController extends AppController {
+
+    public function indexAction(){
+        $this->setMeta('Панель управления');
+
+    }
+
+}
